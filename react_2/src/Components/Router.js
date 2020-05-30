28d5e41074776886057fe9da6a05b55782caa91c
unknown <kwakms821@daum.net>
2020-05-30 13:20:28 +0900

    settimg #5

--- react_2/src/Components/Router.js
+++ react_2/src/Components/Router.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import Home from '../Routes/Home';
+import Home from '../Routes/Home/HomePresenter';
 import Header from '../Components/Header';
-import Search from '../Routes/Search';
-import Movie from '../Routes/Movie';
+import Search from '../Routes/Search/SearchPresenter';
+import Movie from '../Routes/Movie/MoviePresenter';
 
 export default () => (
     <Router>
---
+++ react_2/src/Routes/Home/HomeContainer.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import HomePresenter from './HomePresenter';
+import {moviesApi} from '../../api';
+
+export default class extends React.Component {
+    state = {
+        nowPlaying:null,
+        upcoming:null,
+        popular:null,
+        error:null,
+        loading:true
+    };
+
+    async componentDidMount(){
+        try{
+            const{
+                data:{results: nowPlaying}
+            } = await moviesApi.nowPlaying();
+            const{
+                data:{results: upcoming}
+            } = await moviesApi.upcoming();
+            const{
+                data:{results: popular}
+            } = await moviesApi.popular();
+
+            this.setState( state:{
+                nowPlaying,
+                upcoming,
+                popular
+            })
+        } catch{
+            this.setState( state: {
+                error: "해당 영화 정보를 찾지 못했습니다."
+            });
+        }finally{
+            this.setState(
+                state:{
+                    loading:false
+                }
+            )
+        }
+    }
+
+    render(){
+        const {nowPlaying,upcoming,popular,error,loading} = this.state;
+        return {
+            <HomePresenter
+                nowPlaying={nowPlaying}
+                upcoming={upcoming}
+                popular={popular}
+                error={error}
+                loading={loading}            
+            />
+        }
+    }
+}
