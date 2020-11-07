456d032d50d271e7e958fca0bc647d3ca8eff978
unknown <kwakms821@daum.net>
2020-11-07 23:12:44 +0900

    초기설정,css삽입, jsx

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Hello from './Hello';
+import './css/App.css';
 
 function App() {
+  const name = "name값 입력";
+  const style = {
+    backgroundColor:'black',
+    color:'yellow',
+    fontSize:24,
+    padding:'1rem'
+  }
   return (
     <div>
       <Hello />
       <Hello />
       <Hello />
+      {/* 주석은 이렇게 쓰는거다! */}
+      /* 주석은 이렇게 쓰면 안됨 */
+      <p style={style}>{name}</p>
+      <div className="gray-box"></div>
     </div>
   );
 }
--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Hello(){
-    return <div>안녕하세요</div>
+    return (
+      <>
+        <div>안녕하세요</div>
+        <div>안녕하세요</div>
+      </>
+    );
 }
 
 export default Hello;
