fc846a53de08c5e45f1e1e33c6c16730cfb2a94d
unknown <kwakms821@daum.net>
2020-11-24 00:16:46 +0900

    or복습

--- item2/src/Components/Test1.js
+++ item2/src/Components/Test1.js
@@ -2,8 +2,29 @@ import React, { Component } from 'react'
 
 class Test1 extends Component {
     render() {
+        const title='신상명세서';
+        const name='홍길동';
+        const age=20;
+        const addr='서울';
+        const done=false;
+        const color='green';
+        const style1 = {
+            color:'pink',
+            fontSize:16,
+            backgroundColor:'yellow',
+        };
         return (
             <div>
+                <h2>변수:{} - JSX</h2>
+                <h3 syle={{color,fontSize:20}}>{title}</h3>
+                <ul>
+                    <li style={style1}>이름:{name}</li>
+                    <li>나이:{age}</li>
+                    <li>주소:{addr}</li>
+                    <li>{done === true ? '참':'거짓'}</li>
+                    <li>{done && '참이면 이게나옴'}</li>
+                    <li>{done || '둘중에 하나가 참이면 나옴'}</li>
+                </ul>
                 Test1
             </div>
         )
---
+++ item2/src/Components/Test1Sub.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+
+class Test1Sub extends Component {
+    render() {
+        return <div>Test1Sub</div>;
+    }
+}
+export default Test1Sub;
