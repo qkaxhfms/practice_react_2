1e2853c33bfafcc90bc68a7593e71047ee8c67ec
MS\kwakm <kwakms821@daum.net>
2020-12-03 15:20:52 +0900

    data 주고받기

--- item3/src/App.js
+++ item3/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Test1 from './Components/Test1';
+import Test2 from './Components/Test2';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Test1 />
+      <Test2 />
     </div>
   );
 }
